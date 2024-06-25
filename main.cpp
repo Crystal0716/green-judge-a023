ec720a543c02e9748f6e2f034e968e2702ef5371
若水 <168269104+Crystal0716@users.noreply.github.com>
2024-06-26 01:18:39 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m
+    while (n>+14&&n<=16)
+        if (n=14&&m>=20)
+        {
+         cout<<"YES"<<endl;
+        }
+
+
+    return 0;
+}
